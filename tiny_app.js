80e1c828a50a37c01406dccb96b702f270ab458e
nikolovGeorgi <gnikolov@sfu.ca>
2017-03-15 22:15:55 +0000

    Added Delete option

--- tiny_app.js
+++ tiny_app.js
@@ -36,13 +36,13 @@ app.get("/urls/new", (req, res) => {
 app.get('/urls/:id', (req, res) => {
   const shortUrl = req.params.id;
   const longUrl = urlDatabase[shortUrl];
-  res.render("urls_show", {urlDatabase}); // if i wanna have single id & link => change this line
-  });
+  // const editUrl = req.params.
+  res.render("urls_id", {shortUrl, longUrl}); // if i wanna have single id & link => change this line
+});
 
 app.get('/about', (req, res) => {
   res.render('pages/about');
 });
-
 
 // ---------------------------- CREATE ---------------------------- //
 
@@ -52,7 +52,22 @@ app.post("/urls", (req, res) => {
   res.redirect("/urls/" + shortUrl);
 });
 
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
+});
+
+// app.post('/urls/:id', (req, res) => {
+//   let editUrl = urlDatabase[req.params.editUrl]; //longUrl
+//   console.log(req.params);
+//   res.redirect('/urls/'+ editUrl);
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
+// to consider
+
+// clicking submit button without anything generates a new string and takes us to the new pages
+// delete the short link page after its been deleted => /urls/tacuu after tacuu has been deleted
