6b427eeaae2fbf29c8f29759da3b1ac0e74d5cc7
nikolovGeorgi <gnikolov@sfu.ca>
2017-03-15 21:29:18 -0700

    Update tiny_app.js

--- tiny_app.js
+++ tiny_app.js
@@ -20,24 +20,6 @@ app.locals.title = "TinyApp";
 // ---------------------------- Middlewares ---------------------------- //
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-// app.use(function (req, res, next) {
-//   // check if client sent cookie
-//   var cookie = req.cookies.cookieName;
-//   if (cookie === undefined)
-//   {
-//     // no: set a new cookie
-//     var randomNumber=Math.random().toString();
-//     randomNumber=randomNumber.substring(2,randomNumber.length);
-//     res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
-//     console.log('cookie created successfully');
-//   }
-//   else
-//   {
-//     // yes, cookie was already present
-//     console.log('cookie exists', cookie);
-//   }
-//   next(); // <-- important!
-// });
 // ---------------------------- Retrieve ---------------------------- //
 
 app.get('/', (req, res) => {
