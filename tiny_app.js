0c5c6f6a3e1cec177035449abeb9555cf73cd64c
Georgi <gnikolov@sfu.ca>
2017-03-18 06:50:12 +0000

    fixed a but with persistant cookie id, preventing from passing register page

--- tiny_app.js
+++ tiny_app.js
@@ -1,9 +1,6 @@
-// DEBUG=express:* node tiny_app.js
-
 const dotenv = require('dotenv').config();  //hide important files
-const express = require("express");
-const bodyParser = require("body-parser");
-// const cookieParser = require('cookie-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const generateRandomString = require('./lib/generateShortUrl');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
@@ -14,53 +11,34 @@ const port = process.env.PORT;
 
 // ---------------------------- dataBases ---------------------------- //
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 const userDB = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+  'userRandomID': {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: 'purple-monkey-dinosaur'
   },
- "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
+ 'user2RandomID': {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk'
   }
 }
 let timesVisited = 0;
 // ---------------------------- Configuration ---------------------------- //
 app.set('view engine', 'ejs');
-app.locals.title = "TinyApp";
+app.locals.title = 'TinyApp';
 appTitle = app.locals.title;
 // ---------------------------- Middlewares ---------------------------- //
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
-// app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
   secret: process.env.SESSION_SECRET,
-  // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
-
-// ---------------------------- Custom Middleware ---------------------------- //
-app.use((req, res, next) => {
-  const user = userDB[req.session.email]; // Session Version
-  // If the user is found, add it to the request
-  if (user) {
-    req.user = user;
-  }
-  next();
-});
-function loggedIn(req, res, next) {
-    if (req.user) {
-        next();
-    } else {
-        res.redirect('/login');
-    }
-}
 // ---------------------------- Main Page ---------------------------- //
 app.get('/', (req, res) => {
   // if user is loged in > redirect to urls && if not > to the login page
@@ -75,9 +53,10 @@ app.get('/login', (req, res) => {
   // res.render('login');
   if (req.session.id){
     let templateVars = {
-      userID: req.session.id
+      userID: req.session.id,
     }
     res.redirect('/');
+    return;
   }
   res.render('login');
 });
@@ -86,7 +65,6 @@ app.post('/login', (req, res) => {
   for (let userID in userDB){
     if ((userDB[userID].email === req.body.email) && (userDB[userID].password === req.body.password)){
       req.session.id = userDB[userID].id;
-      console.log(req.session.id, "ID");
       res.redirect('/urls');
       return;
     }
@@ -95,14 +73,13 @@ app.post('/login', (req, res) => {
 });
 // ---------------------------- Register Page ---------------------------- //
 app.get('/register', (req, res) => {
-  if (req.session.id){
-    let templateVars = {
-      userID: req.session.id
-    }
-    res.redirect('/');
-    return;
-  }
-  // render of user_register, templateVars
+  // if (req.session.id){
+  //   let templateVars = {
+  //     userID: req.session.id
+  //   }
+  //   res.redirect('/');
+  //   return;
+  // }
   res.render('register');
 });
 app.post('/register', (req, res) => {
@@ -112,11 +89,13 @@ app.post('/register', (req, res) => {
       const user = userDB[userID];
       // if the user inputs the right email & pass > direct them to urls
       if ((user.email === req.body.email) && (user.password === req.body.password)){
-        res.redirect('/');
+        res.redirect('/login');
+        console.log("redirecting to login----------------------------------------");
         return;
         // if the email is correct, but not the password > forbidden error
       } else if (user.email === req.body.email) {
         res.status(403).render('403');
+        console.log("$$$$$$$$$$$$$$ ERRR 403   $$$$$$$$$$$$$$$");
         return;
       }
     }
@@ -129,33 +108,60 @@ app.post('/register', (req, res) => {
     }
     req.session.id = newUserID;
     res.redirect('/');
+    console.log("redirecting to / ----------------------------------------");
     return;
   }
   res.status(403).render('403');
+  console.log("$$$$$$$$$$$$$$ ERRR 403 BOTTOM  $$$$$$$$$$$$$$$");
 });
+
 app.get('/urls', (req, res) => {
+  let currentUser = userDB[req.session.id];
+  if (!currentUser){
+    //TODO not according to requirements > fix
+    return res.redirect('/register');
+  }
+  var templateVars = {
+    urlDatabase: urlDatabase,
+    userID: req.session.id,
+    userEmail: userDB[req.session.id].email
+  }
+  res.render('./FinalPages/urls_index', templateVars);
+});
+
+app.post('/urls', (req, res) => {
   if (req.session.id){
-    console.log(req.session.email, " hi");
-    console.log(req.session, "session");
-    console.log(urlDatabase, "urlDatabase");
+    console.log(userDB[req.session.id].id);
+    let shortUrl = generateRandomString();
+    urlDatabase[shortUrl] = req.body.longUrl;
+    // urlDatabase[userDB[req.session.id].id] = {
+      // shortUrl: req.body.longUrl,
+
+    console.log(urlDatabase);
+    res.redirect('/urls/' + shortUrl);
+    return;
+  }
+  res.redirect('/login');
+});
+
+app.get('/urls/new', (req, res) => {
+    console.log("------------------------------------------");
+    console.log(userDB[req.session.id].id); // email
+    console.log("------------------------------------------");
+  if (req.session.id){
     var templateVars = {
       urlDatabase: urlDatabase,
       userID: req.session.id,
       userEmail: userDB[req.session.id].email
     }
-    console.log(templateVars);
-    res.render('./FinalPages/urls_index', templateVars);
+    res.render('./FinalPages/urls_new', templateVars);
     return;
   }
   res.redirect('/login');
 });
-app.post("/urls", (req, res) => {
-  if (res.session.id){
-    let shortUrl = generateRandomString();
-    urlDatabase[shortUrl] = req.body.longUrl;
-    res.redirect("/urls/" + shortUrl);
-    return;
-  }
+app.post('/logout', (req, res) => {
+  delete req.session.id;
+  delete req.session.email;
   res.redirect('/login');
 });
 app.get('/urls/:id', (req, res) => {
@@ -163,18 +169,9 @@ app.get('/urls/:id', (req, res) => {
     var templateVars = {
       urlDatabase: urlDatabase,
       userID: req.session.id,
-      userEmail: req.session.email
+      userEmail: userDB[req.session.id].email
     }
-    res.render("./FinalPages/urls_show", templateVars);
-    return;
-  }
-  res.redirect('/login');
-});
-app.get("/urls/new", (req, res) => {
-  console.log("hi");
-  console.log(req.session.id);
-  if (req.session.id){
-    res.render("./FinalPages/urls_new");
+    res.render('./FinalPages/urls_show', templateVars);
     return;
   }
   res.redirect('/login');
@@ -195,7 +192,6 @@ app.post('/urls/:id', (req, res) => {
 });
 app.get('/u/:id', (req, res) => {
     if (req.params.id){
-      console.log(req.params.id);
       res.redirect(`${urlDatabase[req.params.id]}`);
       return;
     }
@@ -210,12 +206,6 @@ app.post('/urls/:id/delete', (req, res) => {
   }
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
-});
-
-app.post('/logout', (req, res) => {
-  delete req.session.id;
-  delete req.session.email;
-  res.redirect('/login');
 });
 // ---------------------------- Ports ---------------------------- //
 app.listen(port, () => {
